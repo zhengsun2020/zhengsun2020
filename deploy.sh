af6e244b885331ca3ae4f49b8a24e64a09357343
zhengsapphire19790319 <71797624+zhengsapphire19790319@users.noreply.github.com>
2020-10-02 11:31:26 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -88,10 +88,10 @@ branch=${GITHUB_REF#refs/heads/}
 #        echo "wget -Oconfig.json https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$branch/config/config_vmess.json"
 #use IBM_APP_NAME alphabet1 + alphabet2 + number1 as pbfile name instead json
         echo "wget -Ozs.pbf https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$branch/config/zs.pbf"
-        sleep 15s
-        rm -rf zs.pbf
+        #sleep 15s
+        #rm -rf zs.pbf
         #rm -rf zs
-        rm -rf zds.sh
+        #rm -rf zds.sh
 #        echo "sed 's/V2_ID/$V2_ID/' config.json -i"
 #        echo "sed 's/V2_PATH/$V2_PATH/' config.json -i"
 #        echo "sed 's/ALTER_ID/$ALTER_ID/' config.json -i"
