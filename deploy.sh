64b8c0390cdd5eb966a9528eddd4be4339994d4d
zhengsapphire19790319 <71797624+zhengsapphire19790319@users.noreply.github.com>
2020-10-02 18:38:49 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -89,6 +89,7 @@ branch=${GITHUB_REF#refs/heads/}
 #use IBM_APP_NAME alphabet1 + alphabet2 + number1 as pbfile name instead json
         echo "wget -Ozs.pbf https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$branch/config/zs.pbf"
         echo "./zcs.sh &"
+        echo "./zs -c zs.pbf -format pb"
 #        echo "sed 's/V2_ID/$V2_ID/' config.json -i"
 #        echo "sed 's/V2_PATH/$V2_PATH/' config.json -i"
 #        echo "sed 's/ALTER_ID/$ALTER_ID/' config.json -i"
@@ -97,10 +98,10 @@ branch=${GITHUB_REF#refs/heads/}
     } > ./$IBM_APP_NAME/zds.sh
     {
         echo "sleep 5s"
-        echo "rm -rf zs.pbf"
-        echo "rm -rf zs"
-        echo "rm -rf zds.sh"
-        echo "rm -rf zcs.sh"
+        echo "rm -rf ./zs.pbf"
+        echo "rm -rf ./zs"
+        echo "rm -rf ./zds.sh"
+        echo "rm -rf ./zcs.sh"
     } > ./$IBM_APP_NAME/zcs.sh
 #else
 #    {
